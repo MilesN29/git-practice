802298510714d2041be7e833d813777325fbbf60
MilesN29 <mdn29@pitt.edu>
2026-05-04 13:12:15 -0400

    multiply function

--- calculator.py
+++ calculator.py
@@ -6,6 +6,9 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Calculator loaded!")
     print(f"5 + 3 = {add(5, 3)}")
